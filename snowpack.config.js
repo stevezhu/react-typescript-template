07c04352a738a941e8133fcb7e6190c52daa2c94
Steve Zhu <st.zhu1@gmail.com>
2020-06-03 00:30:14 -0400

    Change build to use snowpack (#4)

---
+++ snowpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: '@snowpack/app-scripts-react',
+  scripts: {},
+  plugins: [],
+  devOptions: {
+    bundle: false,
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import {render} from 'react-dom';
 import {App} from './App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/#hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
