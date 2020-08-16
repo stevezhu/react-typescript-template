e3c54004bd1895aee66a111cca334b809f8db150
Steve Zhu <st.zhu1@gmail.com>
2020-08-16 15:14:40 -0400

    Bump @storybook/react from 5.3.19 to 6.0.10 (#38)
    Switch back to yarn due to some incompatibilities with snowpack and storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,3 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      use: ['babel-loader', 'react-docgen-typescript-loader'],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
 };
